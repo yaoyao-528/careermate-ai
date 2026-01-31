c631690ca5d81e0197710ec7545597d4c0bece23
Yaoyao Feng <yfen90@gmail.com>
2026-01-31 20:33:40 +1100

    fix

--- src/src/app/components/SignUp/components/Form/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = ({ children }) => (
-  <button className="h-12 w-full rounded-3xl bg-[linear-gradient(98deg,#504ffd_12%,#40c3fb_91%)] text-white text-sm font-bold">
-    {children}
-  </button>
-);
-
-export default Button;
--- src/src/app/components/SignUp/components/Form/components/LoginLink/LoginLink.jsx
+++
@@ -1,9 +0,0 @@
-const LoginLink = () => (
-  <p className="text-center text-sm">
-    <span className="text-gray-700">Already have an account?</span>
-    &nbsp;
-    <span className="text-blue-500">Log in</span>
-  </p>
-);
-
-export default LoginLink;
