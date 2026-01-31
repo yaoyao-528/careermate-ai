2cb9072832e8ce85c99770d1876d567ceb665e77
Yaoyao Feng <yfen90@gmail.com>
2026-01-31 23:01:57 +1100

    update

--- src/app/components/SignUp/components/Form/Form.jsx
+++ src/app/components/SignUp/components/Form/Form.jsx
@@ -6,47 +6,6 @@ import Field from "./components/Field";
 import LoginLink from "./components/LoginLink";
 import { isEmail, isEmpty, isStrongPassword } from "validator";
 
-// setFullName
-
-// 当用户 click 的时候，set fullname 为
-
-// 当 ? 时候，set fullname 为 ?
-// 这里对代码能力的需求为0，对人生经验的需求为 100%
-
-// 当用户输入的时候，set fullname 为用户输入的值
-
-// react 如何拿到用户的输入
-
-// 当 ? 时候，set fullname error 为？
-// 当 submit form 并且 fullname 为空的时候，set fullname error 为 "Please enter your full name"
-// fullNameError 可以从 fullName 状态计算出来
-
-// 当 ？时候，set email error 为 ？
-// 当 submit form 并且 email 为空的时候，set email error 为 "Please enter your email"
-// 当 submit form 并且 email 格式不对的时候，set email error 为 "Please enter a valid email address"
-// emailError 可以从 email 状态计算出来
-
-// 当 submit form 并且 password 为空的时候，set password error 为 "Please enter your password"
-// 当 submit form 并且 password 少于 8 个字符的时候，set password error 为 "Password must be at least 8 characters long"
-// passwordError 可以从 password 状态计算出来
-
-// 当 submit form 的时候
-// state 用来保存 form 被 submit 了
-
-// 衍生状态，很多情况下，一个状态可以从另外一个或者多个状态计算出来
-
-// 电视
-// 电视 有 开关状态 -> 开关指示灯 -> 屏幕显示内容
-// 输入源状态
-// 开关 + 输入源 -> HDMI1 也就是机顶盒内容
-// 屏幕显示内容 -> HDMI2 也就是PS5内容
-// 屏幕显示内容 -> HDMI3 也就是...内容
-
-// 从多个验证中找到检查通过的验证，并返回其错误信息
-// const getError = (value, validators) =>
-//   validators.find((validator) => validator.check(value))?.message || "";
-
-// 从多个错误中找到符合的错误，并返回其错误信息
 const getError = (value, errors) =>
   errors.find((error) => error.match(value))?.message || "";
 
@@ -94,7 +53,7 @@ const Form = () => {
         </p>
       </div>
 
-      <div className="space-y-4">
+      <div className="space-y-8">
         <Field
           value={fullName}
           onChange={(event) => setFullName(event.target.value)}
--- src/app/components/SignUp/components/Form/components/Field/Field.jsx
+++ src/app/components/SignUp/components/Form/components/Field/Field.jsx
@@ -1,21 +1,49 @@
-const Field = ({ label, value, placeholder, onChange, type, error }) => (
-  <div>
-    <div className="mb-2">
-      <label className="text-gray-700 text-sm">{label}</label>
-    </div>
+import { Eye, EyeClosed } from "lucide-react";
+import { useId, useState } from "react";
+import { twMerge } from "tailwind-merge";
+
+const Field = ({ label, value, placeholder, onChange, type, error }) => {
+  const id = useId();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  return (
     <div>
-      <input
-        type={type}
-        value={value}
-        onChange={onChange}
-        className={`px-4 h-12 rounded-3xl border border-gray-300 w-full ${
-          error && "border-red-500"
-        }`}
-        placeholder={placeholder}
-      />
+      <div className="mb-2">
+        <label className="text-gray-700 text-sm" htmlFor={id}>
+          {label}
+        </label>
+      </div>
+      <div className="relative">
+        <input
+          id={id}
+          type={showPassword ? "text" : type}
+          value={value}
+          onChange={onChange}
+          className={twMerge(
+            "px-4 h-12 rounded-3xl border border-gray-300 w-full",
+            error && "border-red-500"
+          )}
+          placeholder={placeholder}
+        />
+        {type === "password" && (
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute top-0 right-0 bottom-0 flex items-center px-4 cursor-pointer text-gray-300"
+          >
+            {showPassword ? <Eye /> : <EyeClosed />}
+          </button>
+        )}
+      </div>
+      <div className="text-sm text-red-500 mt-1 relative">
+        <div className="absolute">{error}</div>
+      </div>
     </div>
-    <div className="text-sm text-red-500 h-[1em] mt-1">{error}</div>
-  </div>
-);
+  );
+};
 
 export default Field;
+
+// 要带着一点完美主义去写代码
+// 1. 代码要简洁 - 恰到好处
